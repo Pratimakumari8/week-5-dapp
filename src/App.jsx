2f376d89bf1afd75e43edb73a47c59b91c438d06
Pratima Kumari <kumaripratima337@gmail.com>
2026-04-24 16:35:31 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,19 @@ import {
 }from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import '@solana/wallet-adapter-react-ui/styles.css';
+import { RequestAirdrop } from './RequestAirdrop';
 function App() {
   return (
-    <>
-      
-    </>
+   <ConnectionProvider endpoint= {"https://api.devnet.solana.com"}>
+    <WalletProvider wallets= {[]} autoConnect>
+      <WalletModalProvider>
+        <div>
+         <WalletMultiButton/>
+         <RequestAirdrop/>
+        </div>
+      </WalletModalProvider>
+    </WalletProvider>
+   </ConnectionProvider>
   )
 }
-
 export default App
